c8371667e0baaed4560c3f717d290c7ea48fa8ef
Wojciech Jarosz <jaroszw@gmail.com>
2021-02-02 15:06:36 +0100

    sliding start

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const slideContainer = document.querySelector(".container");
 const slide = document.querySelector(".slides");
 const nextBtn = document.getElementById("next-btn");
 const prevBtn = document.getElementById("prev-btn");
+const interval = 3000;
 
 let slides = document.querySelectorAll(".slide");
-let index = 1;
+let index = 0;
 
 const firstClone = slides[0].cloneNode(true);
 const lastClone = slides[slides.length - 1].cloneNode(true);
@@ -17,4 +18,20 @@ slide.prepend(lastClone);
 
 const slideWidth = slides[index].clientWidth;
 
-slide.style.transform = `translateX(${-slideWidth * index}px)`;
+const startSlide = () => {
+  setInterval(() => {
+    index++;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+    slide.style.transition = ".3s ease-in-out";
+  }, interval);
+};
+
+slide.addEventListener("transitionend", () => {
+  if (slides[index].id === firstClone.id) {
+    slide.style.transition = "none";
+    index = 1;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  }
+});
+
+startSlide();
