e4f08c8cb0e79672742cd08309e092baddc52b89
Wojciech Jarosz <jaroszw@gmail.com>
2021-02-02 18:32:54 +0100

    to be finished

--- main.js
+++ main.js
@@ -1,37 +1,46 @@
-const slideContainer = document.querySelector(".container");
-const slide = document.querySelector(".slides");
-const nextBtn = document.getElementById("next-btn");
-const prevBtn = document.getElementById("prev-btn");
-const interval = 3000;
+const slideContainer = document.querySelector('.container');
+const slide = document.querySelector('.slides');
+const nextBtn = document.getElementById('next-btn');
+const prevBtn = document.getElementById('prev-btn');
+const interval = 1000;
 
-let slides = document.querySelectorAll(".slide");
-let index = 0;
+let slides = document.querySelectorAll('.slide');
+let index = 1;
+let slideId;
 
 const firstClone = slides[0].cloneNode(true);
 const lastClone = slides[slides.length - 1].cloneNode(true);
 
-firstClone.id = "first-clone";
-lastClone.id = "last-clone";
+firstClone.id = 'first-clone';
+lastClone.id = 'last-clone';
 
 slide.append(firstClone);
 slide.prepend(lastClone);
 
 const slideWidth = slides[index].clientWidth;
+slide.style.transform = `translateX(${-slideWidth * index}px)`;
 
 const startSlide = () => {
-  setInterval(() => {
-    index++;
+  slideId = setInterval(() => {
     slide.style.transform = `translateX(${-slideWidth * index}px)`;
-    slide.style.transition = ".3s ease-in-out";
+    slide.style.transition = '.7s ease-in-out';
+    index++;
   }, interval);
 };
 
-slide.addEventListener("transitionend", () => {
+slide.addEventListener('transitionend', () => {
+  slides = document.querySelectorAll('.slide');
   if (slides[index].id === firstClone.id) {
-    slide.style.transition = "none";
+    slide.style.transition = 'none';
     index = 1;
     slide.style.transform = `translateX(${-slideWidth * index}px)`;
   }
 });
+
+slideContainer.addEventListener('mouseenter', () => {
+  clearInterval(slideId);
+});
+
+slideContainer.addEventListener('mouseleave', startSlide);
 
 startSlide();
