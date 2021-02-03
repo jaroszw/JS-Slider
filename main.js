14d3a8646b8fbde50d1adf82d16dbfca6481da30
Wojciech Jarosz <jaroszw@gmail.com>
2021-02-03 11:41:01 +0100

    final

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const slideContainer = document.querySelector('.container');
-const slide = document.querySelector('.slides');
-const nextBtn = document.getElementById('next-btn');
-const prevBtn = document.getElementById('prev-btn');
-const interval = 1000;
+const slideContainer = document.querySelector(".container");
+const slide = document.querySelector(".slides");
+const nextBtn = document.getElementById("next-btn");
+const prevBtn = document.getElementById("prev-btn");
+const interval = 2000;
 
-let slides = document.querySelectorAll('.slide');
+let slides = document.querySelectorAll(".slide");
 let index = 1;
 let slideId;
 
 const firstClone = slides[0].cloneNode(true);
 const lastClone = slides[slides.length - 1].cloneNode(true);
 
-firstClone.id = 'first-clone';
-lastClone.id = 'last-clone';
+firstClone.id = "first-clone";
+lastClone.id = "last-clone";
 
 slide.append(firstClone);
 slide.prepend(lastClone);
@@ -22,25 +22,50 @@ slide.style.transform = `translateX(${-slideWidth * index}px)`;
 
 const startSlide = () => {
   slideId = setInterval(() => {
-    slide.style.transform = `translateX(${-slideWidth * index}px)`;
-    slide.style.transition = '.7s ease-in-out';
-    index++;
+    moveToNextSlide();
   }, interval);
 };
 
-slide.addEventListener('transitionend', () => {
-  slides = document.querySelectorAll('.slide');
+const getSlides = () => {
+  return (slides = document.querySelectorAll(".slide"));
+};
+
+slide.addEventListener("transitionend", () => {
+  slides = getSlides();
   if (slides[index].id === firstClone.id) {
-    slide.style.transition = 'none';
+    slide.style.transition = "none";
     index = 1;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  }
+  if (slides[index].id === lastClone.id) {
+    slide.style.transition = "none";
+    index = slides.length - 2;
     slide.style.transform = `translateX(${-slideWidth * index}px)`;
   }
 });
 
-slideContainer.addEventListener('mouseenter', () => {
+const moveToNextSlide = () => {
+  getSlides();
+  if (index >= slides.length - 1) return;
+  index++;
+  slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  slide.style.transition = ".7s";
+};
+
+const moveToPrevSlide = () => {
+  if (index <= 0) return;
+  getSlides();
+
+  index--;
+  slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  slide.style.transition = ".7s";
+};
+
+slideContainer.addEventListener("mouseenter", () => {
   clearInterval(slideId);
 });
 
-slideContainer.addEventListener('mouseleave', startSlide);
-
+slideContainer.addEventListener("mouseleave", startSlide);
+nextBtn.addEventListener("click", moveToNextSlide);
+prevBtn.addEventListener("click", moveToPrevSlide);
 startSlide();
